d05970ac50d9a2b2922ea1beffb34ff4e54b4fa9
Adritsheu <atrejosheu@g.harvard.edu>
2021-11-16 21:07:09 -0500

    Updated tests.py file

--- tests.py
+++ tests.py
@@ -1,121 +1,118 @@
-# Start writing code here...
 import pytest
 import numpy as np
-from ../dual_temp import *
-
-tolerance = 0.000000001
-
-class TestForward:
-
-    def test_init(self):
-        with pytest.raises(TypeError):
-            x = dual("Here is a string")
+from src.dual_temp import *
 
 
-    def test_add_radd(self)
 
-    # test addition of two dual numbers
-        x = dual(5, 1)
-        y = dual(8, 3)
-        f = x + y
-        assert f.val == 13
-        assert f.der == 4
-
-    # test reverse addition of two dual numbers
-        f_rev = y + x
-        assert f_rev.val == 13
-        assert f_rev.der == 4
+def test_init():
+    with pytest.raises(TypeError):
+        x = dual("Here is a string")
 
 
-    # test addition of a dual and non-dual number
-        x = 5
-        y = dual(8, 3)
-        f = x + y
-        assert f.val == 13
-        assert f.der == 3
+def test_add_radd():
 
-    # test reverse addition of a dual and non-dual number
-        f_rev = y + x
-        assert f_rev.val == 13
-        assert f_rev.der == 3
+# test addition of two dual numbers
+    x = dual(5, 1)
+    y = dual(8, 3)
+    f = x + y
+    assert f.val == 13, Exception(f"test_add_radd has error with two dual numbers")
+    assert f.der == 4, Exception(f"test_add_radd has error with two dual numbers")
 
-
-    # test addition of x and x**2
-        x = dual(5, 1)
-        y = x*x
-        f = x + y
-        assert f.val == 30
-        assert f.der == 11
+# test reverse addition of two dual numbers
+    f_rev = y + x
+    assert f_rev.val == 13, Exception(f"test_add_radd has error with reverse")
+    assert f_rev.der == 4, Exception(f"test_add_radd has error with reverse")
 
 
-    def test_mul_rmul(self)
-    # test multiplication of two dual numbers
-        x = dual(5, 1)
-        y = dual(8, 1)
-        f = x*y
-        assert f.val == 40
-        assert f.der == 1
+# test addition of a dual and non-dual number
+    x = 5
+    y = dual(8, 3)
+    f = x + y
+    assert f.val == 13, Exception(f"test_add_radd has error with nondual")
+    assert f.der == 3,Exception(f"test_add_radd has error with nondual")
 
-    # test reverse multiplication of two dual numbers
-        f_rev = y*x
-        assert f_rev.val == 40
-        assert f_rev.der == 1
-
-
-    # test multiplication of a dual and non-dual number
-        x = dual(5, 1)
-        y = 3
-        f = x*y
-        assert f.val == 15
-        assert f.der == 1
-
-    # test reverse multiplication of a dual and non-dual number
-        f_rev = y*x
-        assert f_rev.val == 15
-        assert f_rev.der == 1
-
-    # test multiplication of x and x**2
-        x = dual(5,1)
-        y = x*x
-        f = x*y
-        assert f.val == 125
-        assert f.der == 75
+# test reverse addition of a dual and non-dual number
+    f_rev = y + x
+    assert f_rev.val == 13, Exception(f"test_add_radd has error with reverse and nondual")
+    assert f_rev.der == 3, Exception(f"test_add_radd has error with reverse and nondual")
 
 
-    def test_sub_rsub(self):
-    # test subtraction between two dual numbers
-        x = dual(5, 1)
-        y = dual(8, 2)
-        f = x-y
-        assert f.val = -3
-        assert f.der = -1
+# test addition of x and x**2
+    x = dual(5, 1)
+    y = x*x
+    f = x + y
+    assert f.val == 30, Exception(f"test_add_radd has error")
+    assert f.der == 11, Exception(f"test_add_radd has error")
 
-    # test reverse subtraction between two dual numbers
-        f_rev = y-x
-        assert f_rev.val = 3
-        assert f_rev.der = 1
 
-    # test subtraction between a dual and non-dual number
-        x = 7
-        y = dual(5, 1)
-        f = x-y
-        assert f.val = 2
-        assert f.der = -1
+def test_mul_rmul():
+# test multiplication of two dual numbers
+    x = dual(5, 1)
+    y = dual(8, 1)
+    f = x*y
+    assert f.val == 40, Exception(f"test_mul_rmul has error")
+    assert f.der == 1, Exception(f"test_mul_rmul has error")
 
-    # test reverse subtraction between a dual and non-dual number
-        f_rev = y-x
-        assert f_rev.val = 2
-        assert f_rev.der = 1
+# test reverse multiplication of two dual numbers
+    f_rev = y*x
+    assert f_rev.val == 40, Exception(f"test_mul_rmul has error")
+    assert f_rev.der == 1, Exception(f"test_mul_rmul has error")
 
-    # test subtraction between x and x**2
-        x = dual(5, 1)
-        y = x*x
-        f = x - y
-        assert f.val == -20
-        assert f.der == -9
 
-    
-    # test division between a dual number and non-dual number
+# test multiplication of a dual and non-dual number
+    x = dual(5, 1)
+    y = 3
+    f = x*y
+    assert f.val == 15, Exception(f"test_mul_rmul has error")
+    assert f.der == 1, Exception(f"test_mul_rmul has error")
+
+# test reverse multiplication of a dual and non-dual number
+    f_rev = y*x
+    assert f_rev.val == 15, Exception(f"test_mul_rmul has error")
+    assert f_rev.der == 1, Exception(f"test_mul_rmul has error")
+
+# test multiplication of x and x**2
+    x = dual(5,1)
+    y = x*x
+    f = x*y
+    assert f.val == 125, Exception(f"test_mul_rmul has error")
+    assert f.der == 75, Exception(f"test_mul_rmul has error")
+
+
+def test_sub_rsub():
+# test subtraction between two dual numbers
+    x = dual(5, 1)
+    y = dual(8, 2)
+    f = x-y
+    assert f.val == -3, Exception(f"test_sub_rsub has error")
+    assert f.der == -1, Exception(f"test_sub_rsub has error")
+
+# test reverse subtraction between two dual numbers
+    f_rev = y-x
+    assert f_rev.val == 3, Exception(f"test_sub_rsub has error")
+    assert f_rev.der == 1, Exception(f"test_sub_rsub has error")
+
+# test subtraction between a dual and non-dual number
+    x = 7
+    y = dual(5, 1)
+    f = x-y
+    assert f.val == 2, Exception(f"test_sub_rsub has error")
+    assert f.der == -1, Exception(f"test_sub_rsub has error")
+
+# test reverse subtraction between a dual and non-dual number
+    f_rev = y-x
+    assert f_rev.val == 2, Exception(f"test_sub_rsub has error")
+    assert f_rev.der == 1, Exception(f"test_sub_rsub has error")
+
+# test subtraction between x and x**2
+    x = dual(5, 1)
+    y = x*x
+    f = x - y
+    assert f.val == -20, Exception(f"test_sub_rsub has error")
+    assert f.der == -9, Exception(f"test_sub_rsub has error")
+
+
+# test division between a dual number and non-dual number
 #     def test_div_rdiv(self):
 #     x = 2
 #     y = dual(1, 1)
@@ -139,4 +136,4 @@ class TestForward:
 #     assert rf.val = 0.5
 #     assert rf.der =
 
-    
+
