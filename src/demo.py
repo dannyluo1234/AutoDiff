a29b99810cf40960311694750c99b731ed8c94fc
Diana Feng <diana.feng@hotmail.com>
2021-11-17 15:39:26 -0500

    completed a naive implementation of computational graph for a simplified dual class that only includes addition and multiplication

--- src/demo.py
+++ src/demo.py
@@ -1,40 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  1 13:22:24 2021
-@author: Danny Luo
+Created on Tue Nov 16 22:14 2021
+@author: Diana Feng
 """
 
 from dual_temp import dual
-import ad_operations as ad
 
-# Single dimension input case, x=1
+# Multiple dimension input case, x=1, y=7
 x = dual(1)
-f1 = x + 2
-# f2 = x + x
-# print(f1.val, f1.der)
-# print(dir(f1))
-# print(f1.__name__)
+y = dual(7)
+f1 = 2 * y + x * (y + y) * 3
+f2 = 4 * (y + x) * x * (x + 3) + x * y
 f1.print_graph()
-# f2.print_graph
-
-# # Multiple dimension input case, x=1, y=2
-# x = dual(1)
-# y = dual(2)
-# z = dual(-1)
-# f2 = ad.sin(x*y) + x**y -2*x
-# # f3 = z ** (0.5)
-# print(f2.val, f2.der)
-# # print(f3.val, f3.der)
-# print(f1)
-
-# # Calculate derivative in a specific direction (gradient dot product with the direction)
-# # Same set up as above, but with direction (2, 3)
-# x = dual(1,2)
-# y = dual(2,3)
-# f = ad.sin(x*y) + x**y -2*x
-# print(f.val, f.der)
-
-# # Calculating gradient, note that the sin function is the ones we define
-# def f(x, y, z):
-#     return ad.sin(x) + y**3 + x*z
-# # print(get_gradient(f, 3, [1,3,2]))
+f2.print_graph()
--- src/dual_temp.py
+++ src/dual_temp.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 30 17:47:39 2021
-@author: Danny Luo
+Created on Tue Nov 16 22:14 2021
+@author: Diana Feng
 """
 
 import math
@@ -13,43 +13,39 @@ class dual():
             self.val = value
             self.der = direction
             self.graph = graph
-            # print(self.val, self.der)
-            # print(self.graph)
         else:
             raise TypeError
         
     def __add__(self, other):
-        if isinstance(other, dual) :
-            self.graph = self.graph + "Add (" + str(self)
-            return dual(self.val + other.val, self.der + other.der, graph=self.graph)
+        if isinstance(other, dual):
+            out = other.graph + self.graph + " Add (" + f'dual({self.val}, {self.der}), ' + f'dual({other.val}, {other.der})) ->'
+            return dual(self.val + other.val, self.der + other.der, graph=out)
         elif isinstance(other, float) or isinstance(other, int):
-            self.graph = self.graph + "Add (" + str(other) + str(self)
-            return dual(self.val + other, self.der, graph=self.graph)
+            out = self.graph + " Add (" + f'dual({self.val}, {self.der}), ' + str(other) + ") ->"
+            return dual(self.val + other, self.der, graph=out)
         else:
             raise TypeError
             
     def __radd__(self, other):
-        print("radd")
         return self.__add__(other)
     
-    # def __mul__(self, other):
-    #     if isinstance(other, float) or isinstance(other, int):
-    #         self.graph = self.graph + "Multiply ( "
-    #         # print(self.graph)
-    #         return dual(self.val * other, self.der * other, graph=self.graph + "Multiply ( ")
-    #     elif isinstance(other, dual):
-    #         other.graph = other.graph + "Multiply ( "
-    #         # print(other.graph)
-    #         return dual(self.val * other.val, self.der * other.val + self.val * other.der, graph=other.graph + "Multiply ( ")
-    #     else:
-    #         raise TypeError
+    def __mul__(self, other):
+        if isinstance(other, dual):
+            out = other.graph + self.graph + " Multiply (" + f'dual({self.val}, {self.der}), ' + f'dual({other.val}, {other.der})) ->'
+            return dual(self.val * other.val, self.der * other.val + self.val * other.der, graph=out)
+        elif isinstance(other, float) or isinstance(other, int):
+            out = self.graph + " Multiply (" + f'dual({self.val}, {self.der}), ' + str(other) + ") ->"
+            return dual(self.val * other, self.der * other, graph=out)
+        else:
+            raise TypeError
     
-    # def __rmul__(self, other):
-    #     print("rmul")
-    #     return self.__mul__(other)
+    def __rmul__(self, other):
+        return self.__mul__(other)
     
 
     def print_graph(self):
+        self.graph = self.graph + f' dual({self.val}, {self.der})'
         print(self.graph)
+        # pass
 
 
