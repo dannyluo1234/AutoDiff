e206ce8fe86c6c1dd783217dac6803a55e1545f7
Diana Feng <diana.feng@hotmail.com>
2021-12-11 14:25:51 -0500

    adding in str and repr for Reverse, finished docstring for all dunder in Reverse

--- src/cs107_creativename/Reverse.py
+++ src/cs107_creativename/Reverse.py
@@ -61,6 +61,24 @@ class Node():
             return self.der
 
     def __add__(self, other):
+        """
+        Add a Node class object with a real number or another Node class object
+        by creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x + y
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 1)]
+        >>> x.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(self.val + other)
             self.children.append((new_node, 1))
@@ -74,9 +92,46 @@ class Node():
             raise TypeError
             
     def __radd__(self, other):
+        """
+        Add a real number or another Node class object with a Node class object
+        by creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x + y
+        >>> y.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 1)]
+        >>> y.children[0][0] == f # True
+        """
         return self.__add__(other)
     
     def __mul__(self, other):
+        """
+        Mutiply a Node class object with a real number or another Node class
+        object by creating a new Node and appending it to the original Node's
+        children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x * y
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 4)]
+        >>> x.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(self.val * other)
             self.children.append((new_node, other))
@@ -90,9 +145,45 @@ class Node():
             raise TypeError
     
     def __rmul__(self, other):
+        """
+        Mutiply a real number or another Node class by a Node class object by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x * y
+        >>> y.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 3)]
+        >>> y.children[0][0] == f # True
+        """
         return self.__mul__(other)
 
     def __truediv__(self, other):
+        """
+        Divide a Node class object by a real number or another Node class by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x / y
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 0.25)]
+        >>> x.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(self.val / other)
             self.children.append((new_node, 1/other))
@@ -106,6 +197,25 @@ class Node():
             raise TypeError       
 
     def __rtruediv__(self, other):
+        """
+        Divide a real number or another Node class by a Node class object by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x / y
+        >>> y.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>,
+            -0.1875)]
+        >>> y.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(other / self.val)
             self.children.append((new_node, - other / (self.val ** 2)))
@@ -114,6 +224,24 @@ class Node():
             raise TypeError  
 
     def __sub__(self, other):
+        """
+        Subtract a Node class object by a real number or another Node class by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x - y
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 1)]
+        >>> x.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(self.val - other)
             self.children.append((new_node, 1))
@@ -127,6 +255,24 @@ class Node():
             raise TypeError
     
     def __rsub__(self, other):
+        """
+        Subtract a real number or another Node class by a Node class object by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x - y
+        >>> y.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, -1)]
+        >>> y.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(other - self.val)
             self.children.append((new_node, -1))
@@ -135,6 +281,24 @@ class Node():
             raise TypeError  
             
     def __pow__(self, other):
+        """
+        Raise a Node class object by a real number or another Node class by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x ** y
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, 108)]
+        >>> x.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(self.val ** other)
             self.children.append((new_node, other * self.val ** (other-1)))
@@ -148,6 +312,25 @@ class Node():
             raise TypeError         
     
     def __rpow__(self, other):
+        """
+        Raise a real number or another Node class by a Node class object by
+        creating a new Node and appending it to the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = x ** y
+        >>> y.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>,
+            112.28984325071113)]
+        >>> y.children[0][0] == f # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             new_node = Node(other ** self.val)
             self.children.append((new_node, other ** self.val * math.log(other)))
@@ -156,9 +339,41 @@ class Node():
             raise TypeError
     
     def __neg__(self):
+        """
+        Negate a Node class object by multiplying the Node by -1, thus creating
+        a new Node and updating the original Node's children
+
+        Returns
+        ----------
+        Node
+            Returns a Node class object representating the new Node
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> f = -x
+        >>> x.children
+        >>> [(<cs107_creativename.Reverse.Node object at 0x7fab33718670>, -1)]
+        >>> x.children[0][0] == f # True
+        """
         return self.__mul__(-1)
 
     def __lt__(self, other):
+        """
+        Determine whether a Node class is less than a real number or another
+        Node class by compare their values
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x < y # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             return self.val < other
         elif isinstance(other, Node):
@@ -167,6 +382,20 @@ class Node():
             raise TypeError
 
     def __gt__(self, other):
+        """
+        Determine whether a Node class is greater than a real number or another
+        Node class by compare their values
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x > y # False
+        """
         if isinstance(other, float) or isinstance(other, int):
             return self.val > other
         elif isinstance(other, Node):
@@ -175,6 +404,20 @@ class Node():
             raise TypeError
 
     def __le__(self, other):
+        """
+        Determine whether a Node class is less than or equal to a real number or
+        another Node class by compare their values
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x <= y # True
+        """
         if isinstance(other, float) or isinstance(other, int):
             return self.val <= other
         elif isinstance(other, Node):
@@ -183,6 +426,20 @@ class Node():
             raise TypeError    
 
     def __ge__(self, other):
+        """
+        Determine whether a Node class is greater than or equal to a real number
+        or another Node class by compare their values
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x >= y # False
+        """
         if isinstance(other, float) or isinstance(other, int):
             return self.val >= other
         elif isinstance(other, Node):
@@ -191,28 +448,88 @@ class Node():
             raise TypeError
 
     def __eq__(self, other):
+        """
+        Determine whether a Node class is equal to a real number or another Node
+        class by compare their value, derivative, and children
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x == y # False
+        """
         if isinstance(other, Node):
             return self.val == other.val and self.der == other.der and self.children == other.children
         else:
             return False
 
     def __ne__(self, other):
-        return not self.__eq__(other)            
-            
+        """
+        Determine whether a Node class is equal to a real number or another Node
+        class by compare their value, derivative, and children
+
+        Returns
+        ----------
+        bool
+
+        Examples
+        ----------
+        >>> x = Node(3)
+        >>> y = Node(4)
+        >>> x != y # True
+        """
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        """
+        Print out the value, derivative, and a list of children recursively for
+        a Node class object in a simplified form
+
+        Returns
+        ----------
+        String
+
+        Examples
+        ----------
+        >>> x = Dual(3)
+        >>> y = Dual(4)
+        >>> f = x ** y
+        >>> g = x + 7
+        >>> repr(x)
+        >>> 'Node(3, None, [(Node(81, None, []), 108), (Node(10, None, []), 1)])'
+        """
+        return(f'Node({self.val}, {self.der}, {self.children})')
+
+    def __str__(self):
+        """
+        Print out the value, derivative, and a list of children recursively for
+        a Node class object
+
+        Returns
+        ----------
+        String
+
+        Examples
+        ----------
+        >>> x = Dual(3)
+        >>> y = Dual(4)
+        >>> f = x ** y
+        >>> g = x + 7
+        >>> str(x)
+        >>> 'Node(3, None, [(Node(81, None, []), 108), (Node(10, None, []), 1)])'
+        """
+        return self.__repr__
+
 
     # using reverse pass to calculate the gradient of specific variables
     def _AutoDiffR(self, variables):
         self.der = 1
         result = []
         for var in variables:
-            '''
-            try:
-                var_grad = var.get_gradient()
-                result.append(var_grad)
-            except:
-                print("Variable Not specified")
-                return
-            '''
             var_grad = var.get_gradient()
             result.append(var_grad)
         return result
@@ -242,21 +559,3 @@ def AutoDiffR(f_list, val_list):
         return results
     else:
         return AutoDiffR1D(f_list, val_list)
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
